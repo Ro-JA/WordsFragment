ee33435acd0c29650a3aeb8a968adb3e89e15d59
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-12-25 21:41:23 +0900

    correct fragment

--- app/src/main/java/com/example/wordsapp/DetailActivity.kt
+++ app/src/main/java/com/example/wordsapp/DetailActivity.kt
@@ -23,15 +23,6 @@ import com.example.wordsapp.databinding.ActivityDetailBinding
 
 
 class DetailActivity : AppCompatActivity() {
-    /**
-     * Provides global access to these variables from anywhere in the app
-     * via DetailActivity.<variable> without needing to create
-     * a DetailActivity instance.
-     */
-    companion object {
-        const val LETTER = "letter"
-        const val SEARCH_PREFIX = "https://www.google.com/search?q="
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,20 +33,5 @@ class DetailActivity : AppCompatActivity() {
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Retrieve the LETTER from the Intent extras
-        // intent.extras.getString returns String? (String or null)
-        // so toString() guarantees that the value will be a String
-        val letterId = intent?.extras?.getString(LETTER).toString()
-
-        val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = WordAdapter(letterId, this)
-
-        // Adds a [DividerItemDecoration] between items
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        )
-
-        title = getString(R.string.detail_prefix) + " " + letterId
     }
 }
